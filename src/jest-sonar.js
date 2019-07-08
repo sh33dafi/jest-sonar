dd4f1b7d88b375ac853032d59abb970c46b165eb
Yannick Houbrix <yannick.houbrix@gmail.com>
2019-07-08 17:03:39 +0200

    feature(reporter): Added a way to configure the output dir and name

--- src/jest-sonar.js
+++ src/jest-sonar.js
@@ -1,18 +1,34 @@
 const Reporter = require('./reporter');
 const fs = require('fs');
+const path = require('path');
+
+const DEFAULT_OPTIONS = {
+    outputDirectory: null,
+    outputName: 'sonar-report'
+};
 
 class JestSonar {
 
     constructor(globalConfig, options) {
-        this.globalConfig = globalConfig;
-        this.options = options;
+        this.options = this.getOptions(options);
+    }
+
+    getOptions(options) {
+        return Object.assign({}, DEFAULT_OPTIONS, options);
     }
 
     onRunComplete(contexts, results) {
         const reporter = new Reporter();
         const report = reporter.toSonarReport(results);
-        fs.writeFileSync('sonar.xml', report);
-    };
+        const fileName = this.getFileName();
+        fs.mkdirSync(path.dirname(fileName), { recursive: true });
+        fs.writeFileSync(fileName, report);
+
+    }
+
+    getFileName() {
+        return path.resolve(this.options.outputDirectory,this.options.outputName);
+    }
 }
 
 module.exports = JestSonar;
