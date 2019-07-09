c297c54556306c997d4e723ae613ff496171b8d8
Yannick Houbrix <sh33dafi@users.noreply.github.com>
2019-07-09 15:18:02 +0200

    chore: added tests

---
+++ src/reporter.spec.js
@@ -0,0 +1,47 @@
+const Reporter = require('./reporter');
+
+describe('reporter', () => {
+
+    let reporter;
+
+    beforeEach(() => {
+        reporter = new Reporter('/the/root');
+    });
+
+    describe('When calling toSonarReport', () => {
+        test('should map a test result file to a sonar report', () => {
+            const testResults = {
+                testResults: [
+                    {
+                        testFilePath: '/the/root/my-test/my-test.spec.js',
+                        testResults: [
+                            {
+                                fullName: 'When doing this should be ok',
+                                duration: 30,
+                                failureMessages: []
+                            },
+                            {
+                                fullName: 'When this is failing the sonar report should contain the failure',
+                                duration: 10,
+                                failureMessages: ['A failure']
+                            }
+                        ]
+                    },
+                    {
+                        testFilePath: '/the/root/my-second-test.spec.js',
+                        testResults: [
+                            {
+                                fullName: 'When doing this should be ok',
+                                duration: 35,
+                                failureMessages: []
+                            }
+                        ]
+                    }
+                ]
+            };
+
+            expect(reporter.toSonarReport(testResults)).toMatchSnapshot();
+        });
+    });
+
+});
